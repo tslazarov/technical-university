7f5616274282b2f048578797d9d92d8c6a4310e5
Tsvetoslav Lazarov <nearfs@abv.bg>
2017-05-18 20:09:09 +0300

    CHORE // Initial mobile

---
+++ SmarterThanYou/SmarterThanYou.Mobile/ViewModels/LoginViewModel.cs
@@ -0,0 +1,51 @@
+﻿using SmarterThanYou.Mobile.Common;
+using SmarterThanYou.Mobile.Models;
+using SmarterThanYou.Mobile.Views;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace SmarterThanYou.Mobile.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private User currentUser;
+        private Frame frame;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LoginViewModel(Frame frame)
+        {
+            this.currentUser = new User();
+            this.frame = frame;
+        }
+
+        public User CurrentUser
+        {
+            get
+            {
+                return currentUser;
+            }
+            set
+            {
+                currentUser = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName]String propName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public void LoginUser()
+        {
+            //Create a call to login a user
+        }
+    }
+}
---
+++ SmarterThanYou/SmarterThanYou.Mobile/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,49 @@
+﻿using SmarterThanYou.Mobile.Common;
+using SmarterThanYou.Mobile.Models;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace SmarterThanYou.Mobile.ViewModels
+{
+    public class RegisterViewModel : INotifyPropertyChanged
+    {
+        private User currentUser;
+        private ICommand registerUserCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RegisterViewModel()
+        {
+            this.currentUser = new User();
+        }
+
+        public User CurrentUser
+        {
+            get
+            {
+                return currentUser;
+            }
+            set
+            {
+                currentUser = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName]String propName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public void RegisterUser()
+        {
+            // Create a call to register
+        }
+    }
+}
